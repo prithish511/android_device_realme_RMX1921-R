45a7a6d7da62a13a3025631c4e505710d737bcaf
Sagarrokade006 <sagarrokade006@gmail.com>
2020-08-30 15:55:25 +0000

    RMX1921: RealmeParts: Update RealmeParts Vector Icon
    - Add RealmeParts Package to Build in device.mk
    
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -473,3 +473,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# RealmeParts
+PRODUCT_PACKAGES += \
+    RealmeParts
+
