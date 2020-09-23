34bed475dbc648c1f3b9e6c3d7741495ff74c57a
improbir <ghosh_probir@yahoo.com>
2020-09-23 20:09:33 +0000

    RMX1921: Add Proxy & light sensor permission
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -399,6 +399,14 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service.realme_sdm710 \
     libsensorndkbridge
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
