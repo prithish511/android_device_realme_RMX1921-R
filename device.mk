edc178885653944842decac12d4d47bb2702a294
improbir <ghosh_probir@yahoo.com>
2020-11-28 11:02:24 +0000

    Init PixelExperience
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -147,7 +147,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.realme_sdm710 \
     vendor.oppo.hardware.biometrics.fingerprint@2.1
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     vendor/evolution/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:vendor/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # FM
