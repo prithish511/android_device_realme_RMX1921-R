1c60fa3f10402164aa65cb0c87218effef171da2
improbir <ghosh_probir@yahoo.com>
2021-01-22 18:16:44 +0000

    RMX1921: Change SEpolicy dirs
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,10 +179,9 @@ TARGET_USE_AOSP_SURFACEFLINGER := true
 VENDOR_SECURITY_PATCH := 2020-06-05
 
 # Sepolicy
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # System-as-root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
