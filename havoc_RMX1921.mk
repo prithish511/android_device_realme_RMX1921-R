b7e10370287ec1b9157b5421a19504890c3811a0
improbir <ghosh_probir@yahoo.com>
2021-02-03 05:30:40 +0000

    RMX1921: switch to Redfin Build FP
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- havoc_RMX1921.mk
+++ havoc_RMX1921.mk
@@ -21,10 +21,10 @@ PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys" \
+        PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys" \
         PRODUCT_NAME="RMX1921" \
 	TARGET_DEVICE="RMX1921"
     
